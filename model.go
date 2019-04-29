f7c0a2f70461621a33af0d8807874c9eea756e1f
fuji <fuji8@users.noreply.github.com>
2019-04-30 03:53:48 +0900

    DB再設計

--- model.go
+++ model.go
@@ -23,29 +23,35 @@ type Room struct {
 	ID        int       `json:"id" gorm:"primary_key; AUTO_INCREMENT"`
 	Place     string    `json:"place"`
 	Date      string    `json:"date" gorm:"type:DATE"`
-	TimeStart string    `json:"time_start"`
-	TimeEnd   string    `json:"time_end"`
+	TimeStart string    `json:"time_start" gorm:"type:TIME"`
+	TimeEnd   string    `json:"time_end" gorm:"type:TIME"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
 // Group グループ情報
 type Group struct {
-	ID        int       `json:"id" gorm:"primary_key; AUTO_INCREMENT"`
-	Name      string    `json:"name" gorm:"unique"`
-	Members   []User    `json:"members" gorm:"many2many:groups_users"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID             int       `json:"id" gorm:"primary_key; AUTO_INCREMENT"`
+	Name           string    `json:"name" gorm:"unique"`
+	Members        []User    `json:"members" gorm:"many2many:group_users"`
+	CreatedBy      User      `json:"created_by" gorm:"foreignkey:CreatedByRefer"`
+	CreatedByRefer string    `json:"created_by_refer"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
 }
 
 // Reservation 予約情報
 type Reservation struct {
-	ID        int       `json:"id" gorm:"AUTO_INCREMENT"`
-	GroupID   int       `json:"group_id"`
-	RoomID    int       `json:"room_id"`
-	Date      string    `json:"date" gorm:"type:DATE"`
-	TimeStart string    `json:"time_start"`
-	TimeEnd   string    `json:"time_end"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID             int       `json:"id" gorm:"AUTO_INCREMENT"`
+	GroupID        int       `json:"group_id"`
+	Group          Group     `json:"group" gorm:"foreignkey:group_id"`
+	RoomID         int       `json:"room_id"`
+	Room           Room      `json:"room" gorm:"foreignkey:room_id"`
+	Date           string    `json:"date" gorm:"type:DATE"`
+	TimeStart      string    `json:"time_start" gorm:"type:TIME"`
+	TimeEnd        string    `json:"time_end" gorm:"type:TIME"`
+	CreatedBy      User      `json:"created_by" gorm:"foreignkey:CreatedByRefer"`
+	CreatedByRefer string    `json:"created_by_refer"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
 }
