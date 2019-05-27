b2d768148205becd322673bef2bc305911703253
fuji <fuji8@users.noreply.github.com>
2019-05-27 12:50:41 +0900

    not null

--- model.go
+++ model.go
@@ -32,10 +32,10 @@ type Room struct {
 // Group グループ情報
 type Group struct {
 	ID             int       `json:"id" gorm:"primary_key; AUTO_INCREMENT"`
-	Name           string    `json:"name" gorm:"unique"`
+	Name           string    `json:"name" gorm:"unique;not null"`
 	Description    string    `json:"description"`
 	Members        []User    `json:"members" gorm:"many2many:group_users"`
-	CreatedBy      User      `json:"created_by" gorm:"foreignkey:CreatedByRefer"`
+	CreatedBy      User      `json:"created_by" gorm:"foreignkey:CreatedByRefer; not null"`
 	CreatedByRefer string    `json:"created_by_refer"`
 	CreatedAt      time.Time `json:"created_at"`
 	UpdatedAt      time.Time `json:"updated_at"`
@@ -44,16 +44,16 @@ type Group struct {
 // Reservation 予約情報
 type Reservation struct {
 	ID             int       `json:"id" gorm:"AUTO_INCREMENT"`
-	Name           string    `json:"name"`
+	Name           string    `json:"name" gorm:"not null"`
 	Description    string    `json:"description"`
-	GroupID        int       `json:"group_id"`
+	GroupID        int       `json:"group_id" gorm:"not null"`
 	Group          Group     `json:"group" gorm:"foreignkey:group_id"`
-	RoomID         int       `json:"room_id"`
+	RoomID         int       `json:"room_id" gorm:"not null"`
 	Room           Room      `json:"room" gorm:"foreignkey:room_id"`
 	Date           string    `json:"date" gorm:"type:DATE"`
 	TimeStart      string    `json:"time_start" gorm:"type:TIME"`
 	TimeEnd        string    `json:"time_end" gorm:"type:TIME"`
-	CreatedBy      User      `json:"created_by" gorm:"foreignkey:CreatedByRefer"`
+	CreatedBy      User      `json:"created_by" gorm:"foreignkey:CreatedByRefer; not null"`
 	CreatedByRefer string    `json:"created_by_refer"`
 	CreatedAt      time.Time `json:"created_at"`
 	UpdatedAt      time.Time `json:"updated_at"`
