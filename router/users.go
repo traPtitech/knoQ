fe5ecb097f130f0ca32513f97aaf6bf307997122
fuji-U <haya10.88ha@gmail.com>
2020-01-04 02:48:44 +0900

    authenticate session auth

--- router/users.go
+++ router/users.go
@@ -3,13 +3,27 @@ package router
 import (
 	"net/http"
 	repo "room/repository"
+	"room/utils"
 
 	"github.com/labstack/echo/v4"
 )
 
 // HandleGetUserMe ヘッダー情報からuser情報を取得
+// 認証状態を確認
 func HandleGetUserMe(c echo.Context) error {
 	requestUser := getRequestUser(c)
+	_, err := utils.GetUserMe(requestUser.Auth)
+	if err != nil {
+		if err.Error() == http.StatusText(http.StatusUnauthorized) {
+			// 認証が切れている
+			if err = repo.DeleteAuth(requestUser.Auth); err != nil {
+				return judgeErrorResponse(err)
+			}
+			return unauthorized(message("Your auth is expired"))
+		}
+		return internalServerError()
+	}
+
 	return c.JSON(http.StatusOK, requestUser)
 }
 
