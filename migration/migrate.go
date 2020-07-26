ebb652d4eb93de3910c3d54ed288a982ee270800
fuji-U <haya10.88ha@gmail.com>
2020-07-26 22:53:32 +0900

    consider existing database

--- migration/migrate.go
+++ migration/migrate.go
@@ -10,7 +10,17 @@ func Migrate(db *gorm.DB, tables []interface{}) error {
 	m := gormigrate.New(db, gormigrate.DefaultOptions, Migrations())
 
 	m.InitSchema(func(tx *gorm.DB) error {
-		err := tx.AutoMigrate(tables...).Error
+		mv1 := gormigrate.New(tx, gormigrate.DefaultOptions, []*gormigrate.Migration{
+			{
+				ID:      "assume existing DB",
+				Migrate: v1().Migrate,
+			},
+		})
+		err := mv1.Migrate()
+		if err != nil {
+			return err
+		}
+		err = tx.AutoMigrate(tables...).Error
 		if err != nil {
 			return err
 		}
