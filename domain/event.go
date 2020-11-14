38c1b4fa182f5cf19aeede9f5952a2cd6b2b2cfd
fuji-U <haya10.88ha@gmail.com>
2020-11-14 15:04:44 +0900

    fix domain

--- domain/event.go
+++ domain/event.go
@@ -6,18 +6,27 @@ import (
 	"github.com/gofrs/uuid"
 )
 
+type Model struct {
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt time.Time
+}
+
 type Event struct {
-	ID          uuid.UUID
-	Name        string
-	Description string
-	Room        Room
-	Group       Group
-	TimeStart   time.Time
-	TimeEnd     time.Time
-	CreatedBy   User
-	Tags        []struct {
-		Tag
-		Locked bool
-	}
+	ID            uuid.UUID
+	Name          string
+	Description   string
+	Room          Room
+	Group         Group
+	TimeStart     time.Time
+	TimeEnd       time.Time
+	CreatedBy     User
+	Tags          []EventTag
 	AllowTogether bool
+	Model
+}
+
+type EventTag struct {
+	Tag
+	Locked bool
 }
