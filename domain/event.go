e4f208a7576940922372b94c4be5daa01242ba5b
fuji-U <haya10.88ha@gmail.com>
2020-11-14 15:26:32 +0900

    event repository interface

--- domain/event.go
+++ domain/event.go
@@ -30,3 +30,49 @@ type EventTag struct {
 	Tag
 	Locked bool
 }
+
+// WriteEventParams is used create and update
+type WriteEventParams struct {
+	Name          string
+	Description   string
+	GroupID       uuid.UUID
+	RoomID        uuid.UUID
+	TimeStart     time.Time
+	TimeEnd       time.Time
+	CreatedBy     uuid.UUID
+	AllowTogether bool
+	Tags          []struct {
+		Name   string
+		Locked bool
+	}
+}
+
+// WriteTagRelationParams is used create and update
+type WriteTagRelationParams struct {
+	ID     uuid.UUID
+	Locked bool
+}
+
+// EventRepository is implemented by ...
+type EventRepository interface {
+	CreateEvent(eventParams WriteEventParams) (*Event, error)
+
+	UpdateEvent(eventID uuid.UUID, eventParams WriteEventParams) (*Event, error)
+	AddTagToEvent(eventID uuid.UUID, tagID uuid.UUID, locked bool) error
+
+	DeleteEvent(eventID uuid.UUID) error
+	// DeleteTagInEvent delete a tag in that Event
+	DeleteTagInEvent(eventID uuid.UUID, tagID uuid.UUID, deleteLocked bool) error
+
+	GetEvent(eventID uuid.UUID) (*Event, error)
+
+	// TODO 一つにまとめる
+	GetAllEvents(start *time.Time, end *time.Time) ([]*Event, error)
+	GetEventsByGroupIDs(groupIDs []uuid.UUID) ([]*Event, error)
+	GetEventsByRoomIDs(roomIDs []uuid.UUID) ([]*Event, error)
+
+	GetEventActivities(day int) ([]*Event, error)
+	// GetEventsByFilter allows you to filter the events under any condition.
+	// However, you may get an error at runtime.
+	GetEventsByFilter(query string, args []interface{}) ([]*Event, error)
+}
