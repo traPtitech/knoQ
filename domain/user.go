6da7f92219eb38b99d61091e8faa0e33e488b1f2
fuji-U <haya10.88ha@gmail.com>
2020-11-14 15:04:56 +0900

    add domain.user

--- domain/user.go
+++ domain/user.go
@@ -3,7 +3,9 @@ package domain
 import "github.com/gofrs/uuid"
 
 type User struct {
-	ID         uuid.UUID
-	Privileged bool
-	IsTrap     bool
+	ID          uuid.UUID
+	Name        string
+	DisplayName string
+	Privileged  bool
+	IsTrap      bool
 }
