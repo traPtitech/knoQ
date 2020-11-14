bb46d46ac37a20616b4488bcfbd036cd12641d90
fuji-U <haya10.88ha@gmail.com>
2020-11-14 15:05:13 +0900

    sample presentation

---
+++ presentation/event.go
@@ -0,0 +1,88 @@
+package presentation
+
+import (
+	"fmt"
+	"room/domain"
+	"time"
+
+	"github.com/gofrs/uuid"
+	"github.com/lestrrat-go/ical"
+)
+
+type EventReqWrite struct {
+	Name          string    `json:"name"`
+	Description   string    `json:"description"`
+	AllowTogether bool      `json:"sharedRoom"`
+	TimeStart     time.Time `json:"timeStart"`
+	TimeEnd       time.Time `json:"timeEnd"`
+	RoomID        uuid.UUID `json:"roomId"`
+	GroupID       uuid.UUID `json:"groupId"`
+	Tags          []struct {
+		Name   string `json:"name"`
+		Locked bool   `json:"locked"`
+	} `json:"tags"`
+}
+
+// EventResOne is experimental
+type EventResOne struct {
+	EventResMulti
+	Place     string
+	GroupName string
+}
+
+type EventResMulti struct {
+	ID            uuid.UUID         `json:"eventId"`
+	Name          string            `json:"name"`
+	Description   string            `json:"description"`
+	AllowTogether bool              `json:"sharedRoom"`
+	TimeStart     time.Time         `json:"timeStart"`
+	TimeEnd       time.Time         `json:"timeEnd"`
+	RoomID        uuid.UUID         `json:"roomId"`
+	GroupID       uuid.UUID         `json:"groupId"`
+	Tags          []domain.EventTag `json:"tags"`
+	CreatedBy     uuid.UUID         `json:"createdBy"`
+	CreatedAt     time.Time         `json:"createdAt"`
+	UpdatedAt     time.Time         `json:"updatedAt"`
+	DeletedAt     *time.Time        `json:"deletedAt,omitempty"`
+}
+
+func iCalVeventFormat(e *domain.Event, host string) *ical.Event {
+	timeLayout := "20060102T150405Z"
+	vevent := ical.NewEvent()
+	vevent.AddProperty("uid", e.ID.String())
+	vevent.AddProperty("dtstamp", time.Now().UTC().Format(timeLayout))
+	vevent.AddProperty("dtstart", e.TimeStart.UTC().Format(timeLayout))
+	vevent.AddProperty("dtend", e.TimeEnd.UTC().Format(timeLayout))
+	vevent.AddProperty("created", e.CreatedAt.UTC().Format(timeLayout))
+	vevent.AddProperty("last-modified", e.UpdatedAt.UTC().Format(timeLayout))
+	vevent.AddProperty("summary", e.Name)
+	e.Description += "\n\n"
+	e.Description += "-----------------------------------\n"
+	e.Description += "イベント詳細ページ\n"
+	e.Description += fmt.Sprintf("%s/events/%v", host, e.ID)
+	vevent.AddProperty("description", e.Description)
+	vevent.AddProperty("location", e.Room.Place)
+	vevent.AddProperty("organizer", e.CreatedBy.DisplayName)
+
+	return vevent
+}
+
+func iCalFormat(events []*domain.Event, host string) *ical.Calendar {
+	c := ical.New()
+	ical.NewEvent()
+	tz := ical.NewTimezone()
+	tz.AddProperty("TZID", "Asia/Tokyo")
+	std := ical.NewStandard()
+	std.AddProperty("TZOFFSETFROM", "+9000")
+	std.AddProperty("TZOFFSETTO", "+9000")
+	std.AddProperty("TZNAME", "JST")
+	std.AddProperty("DTSTART", "19700101T000000")
+	tz.AddEntry(std)
+	c.AddEntry(tz)
+
+	for _, e := range events {
+		vevent := iCalVeventFormat(e, host)
+		c.AddEntry(vevent)
+	}
+	return c
+}
