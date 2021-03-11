909fcfd51e3a0c41ef89d1a0822a091f5d2d8044
fuji <haya10.88ha@gmail.com>
2021-03-11 18:02:03 +0900

    add traq

--- dao/dao.go
+++ dao/dao.go
@@ -1,7 +1,11 @@
 package dao
 
-import "github.com/traPtitech/knoQ/dao/infra/db"
+import (
+	"github.com/traPtitech/knoQ/dao/infra/db"
+	"github.com/traPtitech/knoQ/dao/infra/traq"
+)
 
 type Dao struct {
 	gormRepo db.GormRepository
+	traQRepo traq.TraQRepository
 }
