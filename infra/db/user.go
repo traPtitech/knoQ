cfed926767e8b442b945f0242615034e8af6d314
fuji <haya10.88ha@gmail.com>
2021-03-26 22:20:32 +0900

    fix order

--- infra/db/user.go
+++ infra/db/user.go
@@ -7,38 +7,12 @@ import (
 	"gorm.io/gorm"
 )
 
-// saveUser user.IcalSecret == "" の時、値は更新されません。
-// また、user.Provider, user.Tokenは空の時、更新されません。
-// user.Previlegeは常に更新されません。
-func saveUser(db *gorm.DB, user *User) (*User, error) {
-	err := db.Transaction(func(tx *gorm.DB) error {
-		existingUser, err := getUser(tx.Preload("Provider").Preload("Token"), user.ID)
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return tx.Create(&user).Error
-		}
-		if err != nil {
-			return err
-		}
-
-		if user.IcalSecret == "" {
-			user.IcalSecret = existingUser.IcalSecret
-		}
-		return tx.Session(&gorm.Session{FullSaveAssociations: true}).Save(&user).Error
-	})
-
-	return user, err
-}
-
 func (repo *GormRepository) SaveUser(user User) (*User, error) {
 	return saveUser(repo.db, &user)
 }
 
 func (repo *GormRepository) UpdateiCalSecret(userID uuid.UUID, secret string) error {
 	return updateiCalSecret(repo.db, userID, secret)
-}
-
-func updateiCalSecret(db *gorm.DB, userID uuid.UUID, secret string) error {
-	return db.Model(&User{}).Update("ical_secret", secret).Error
 }
 
 func (repo *GormRepository) Privilege(userID uuid.UUID) bool {
@@ -49,14 +23,6 @@ func (repo *GormRepository) Privilege(userID uuid.UUID) bool {
 	return user.Privilege
 }
 
-func getUser(db *gorm.DB, userID uuid.UUID) (*User, error) {
-	user := User{
-		ID: userID,
-	}
-	err := db.Take(&user).Error
-	return &user, err
-}
-
 func (repo *GormRepository) GetUser(userID uuid.UUID) (*User, error) {
 	return getUser(repo.db.Preload("Provider"), userID)
 }
@@ -64,23 +30,6 @@ func (repo *GormRepository) GetUser(userID uuid.UUID) (*User, error) {
 func (repo *GormRepository) GetAllUsers(onlyActive bool) ([]*User, error) {
 	return getAllUsers(repo.db, onlyActive)
 }
-
-func getAllUsers(db *gorm.DB, onlyActive bool) ([]*User, error) {
-	users := make([]*User, 0)
-	if onlyActive {
-		err := db.Where("state = ?", 1).Find(&users).Error
-		return users, err
-	}
-	err := db.Find(&users).Error
-	return users, err
-}
-
-// func (repo *GormRepository) UpdateUserState(userID uuid.UUID, state int) error {
-// user := User{
-// ID: userID,
-// }
-// return repo.db.Model(&user).Update("state", state).Error
-// }
 
 func (repo *GormRepository) SyncUsers(users []*User) error {
 	err := repo.db.Transaction(func(tx *gorm.DB) error {
@@ -117,4 +66,48 @@ func (repo *GormRepository) SyncUsers(users []*User) error {
 	})
 
 	return err
+}
+
+// saveUser user.IcalSecret == "" の時、値は更新されません。
+// また、user.Provider, user.Tokenは空の時、更新されません。
+// user.Previlegeは常に更新されません。
+func saveUser(db *gorm.DB, user *User) (*User, error) {
+	err := db.Transaction(func(tx *gorm.DB) error {
+		existingUser, err := getUser(tx.Preload("Provider").Preload("Token"), user.ID)
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return tx.Create(&user).Error
+		}
+		if err != nil {
+			return err
+		}
+
+		if user.IcalSecret == "" {
+			user.IcalSecret = existingUser.IcalSecret
+		}
+		return tx.Session(&gorm.Session{FullSaveAssociations: true}).Save(&user).Error
+	})
+
+	return user, err
+}
+
+func updateiCalSecret(db *gorm.DB, userID uuid.UUID, secret string) error {
+	return db.Model(&User{}).Update("ical_secret", secret).Error
+}
+
+func getUser(db *gorm.DB, userID uuid.UUID) (*User, error) {
+	user := User{
+		ID: userID,
+	}
+	err := db.Take(&user).Error
+	return &user, err
+}
+
+func getAllUsers(db *gorm.DB, onlyActive bool) ([]*User, error) {
+	users := make([]*User, 0)
+	if onlyActive {
+		err := db.Where("state = ?", 1).Find(&users).Error
+		return users, err
+	}
+	err := db.Find(&users).Error
+	return users, err
 }
