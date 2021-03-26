91f024de9c268ea054c1c1b13d687a47a862f876
fuji <haya10.88ha@gmail.com>
2021-03-26 19:42:57 +0900

    fix filter

--- usecase/production/event.go
+++ usecase/production/event.go
@@ -1,7 +1,9 @@
 package production
 
 import (
+	"github.com/gofrs/uuid"
 	"github.com/traPtitech/knoQ/domain"
+	"github.com/traPtitech/knoQ/domain/filter"
 	"github.com/traPtitech/knoQ/infra/db"
 )
 
@@ -26,4 +28,76 @@ func (repo *Repository) CreateEvent(params domain.WriteEventParams, info *domain
 	}
 	e := db.ConvertEventTodomainEvent(*event)
 	return &e, nil
+}
+
+func (repo *Repository) UpdateEvent(eventID uuid.UUID, eventParams domain.WriteEventParams, info *domain.ConInfo) (*domain.Event, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (repo *Repository) AddTagToEvent(eventID uuid.UUID, tagID uuid.UUID, locked bool, info *domain.ConInfo) error {
+	panic("not implemented") // TODO: Implement
+}
+
+func (repo *Repository) DeleteEvent(eventID uuid.UUID, info *domain.ConInfo) error {
+	panic("not implemented") // TODO: Implement
+}
+
+// DeleteTagInEvent delete a tag in that Event
+func (repo *Repository) DeleteTagInEvent(eventID uuid.UUID, tagID uuid.UUID, info *domain.ConInfo) error {
+	panic("not implemented") // TODO: Implement
+}
+
+func (repo *Repository) GetEvent(eventID uuid.UUID) (*domain.Event, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+//go:generate gotypeconverter -s v3.UserGroup -d domain.Group -o converter.go .
+
+func (repo *Repository) GetEvents(expr filter.Expr, info *domain.ConInfo) ([]*domain.Event, error) {
+	expr = addTraQGroupIDs(repo, info.ReqUserID, expr)
+
+	_, err := repo.gormRepo.GetAllEvents(expr)
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+func addTraQGroupIDs(repo *Repository, userID uuid.UUID, expr filter.Expr) filter.Expr {
+	t, err := repo.gormRepo.GetToken(userID)
+	if err != nil {
+		return expr
+	}
+
+	var fixExpr func(filter.Expr) filter.Expr
+
+	fixExpr = func(expr filter.Expr) filter.Expr {
+		switch e := expr.(type) {
+		case *filter.CmpExpr:
+			if e.Attr == filter.User {
+				id, ok := e.Value.(uuid.UUID)
+				if !ok {
+					return e
+				}
+				groupIDs, err := repo.traQRepo.GetUserBelongingGroupIDs(t, id)
+				if err != nil {
+					return e
+				}
+				return &filter.LogicOpExpr{
+					LogicOp: filter.Or,
+					Lhs:     e,
+					Rhs:     filter.FilterGroupIDs(groupIDs),
+				}
+			}
+		case *filter.LogicOpExpr:
+			return &filter.LogicOpExpr{
+				LogicOp: e.LogicOp,
+				Lhs:     fixExpr(e.Lhs),
+				Rhs:     fixExpr(e.Rhs),
+			}
+		}
+		return nil
+	}
+	return fixExpr(expr)
 }
