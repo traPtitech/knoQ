845c0be960e74eca16e23dfb490b07f1da810fb9
fuji <haya10.88ha@gmail.com>
2021-03-26 13:47:41 +0900

    add filter

--- domain/filter/event.go
+++ domain/filter/event.go
@@ -1,32 +1,10 @@
 package filter
 
-import "github.com/gofrs/uuid"
+import (
+	"time"
 
-// Expression
-
-func FilterRoomIDs(roomIDs []uuid.UUID) Expr {
-	if len(roomIDs) == 0 {
-		return nil
-	}
-
-	var expr Expr
-	expr = CmpExpr{
-		Attr:     Room,
-		Relation: Eq,
-		Value:    roomIDs[0],
-	}
-	for _, id := range roomIDs[1:] {
-		lhs := expr
-		rhs := CmpExpr{
-			Attr:     Room,
-			Relation: Eq,
-			Value:    id,
-		}
-		expr = LogicOpExpr{Or, lhs, rhs}
-	}
-
-	return expr
-}
+	"github.com/gofrs/uuid"
+)
 
 type Relation int
 
@@ -77,3 +55,77 @@ type CmpExpr struct {
 
 func (LogicOpExpr) isExpr() {}
 func (CmpExpr) isExpr()     {}
+
+func FilterRoomIDs(roomIDs []uuid.UUID) Expr {
+	if len(roomIDs) == 0 {
+		return nil
+	}
+
+	var expr Expr
+	expr = CmpExpr{
+		Attr:     Room,
+		Relation: Eq,
+		Value:    roomIDs[0],
+	}
+	for _, id := range roomIDs[1:] {
+		lhs := expr
+		rhs := CmpExpr{
+			Attr:     Room,
+			Relation: Eq,
+			Value:    id,
+		}
+		expr = LogicOpExpr{Or, lhs, rhs}
+	}
+	return expr
+}
+
+func FilterGroupIDs(groupIDs []uuid.UUID) Expr {
+	if len(groupIDs) == 0 {
+		return nil
+	}
+
+	var expr Expr
+	expr = CmpExpr{
+		Attr:     Group,
+		Relation: Eq,
+		Value:    groupIDs[0],
+	}
+	for _, id := range groupIDs[1:] {
+		lhs := expr
+		rhs := CmpExpr{
+			Attr:     Group,
+			Relation: Eq,
+			Value:    id,
+		}
+		expr = LogicOpExpr{Or, lhs, rhs}
+	}
+	return expr
+}
+
+func FilterTime(start, end time.Time) Expr {
+	if start.IsZero() && end.IsZero() {
+		return nil
+	}
+	timeStart := CmpExpr{
+		Attr:     TimeStart,
+		Relation: GreterEq,
+		Value:    start,
+	}
+	timeEnd := CmpExpr{
+		Attr:     TimeEnd,
+		Relation: LessEq,
+		Value:    end,
+	}
+
+	if start.IsZero() {
+		return timeEnd
+	}
+	if end.IsZero() {
+		return timeStart
+	}
+	return LogicOpExpr{
+		LogicOp: And,
+		Lhs:     timeStart,
+		Rhs:     timeEnd,
+	}
+}
