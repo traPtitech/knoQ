7752f31a6eeaa0da6c73def8fccdab42787ff5a9
fuji <haya10.88ha@gmail.com>
2021-04-03 15:42:50 +0900

    add createGroup test

--- infra/db/group_test.go
+++ infra/db/group_test.go
@@ -4,24 +4,33 @@ import (
 	"testing"
 
 	"github.com/gofrs/uuid"
-	"github.com/stretchr/testify/assert"
+	"github.com/jinzhu/copier"
 	"github.com/traPtitech/knoQ/domain"
 )
 
 func Test_createGroup(t *testing.T) {
+	r, assert, require, user := setupRepoWithUser(t, common)
 
-	r, _, _, user := setupRepoWithUser(t, common)
-
-	group, err := createGroup(r.db, writeGroupParams{
+	params := writeGroupParams{
 		CreatedBy: user.ID,
 		WriteGroupParams: domain.WriteGroupParams{
 			Name:    "first group",
 			Members: []uuid.UUID{user.ID},
 			Admins:  []uuid.UUID{user.ID},
 		},
+	}
+
+	t.Run("create group", func(t *testing.T) {
+		group, err := createGroup(r.db, params)
+		require.NoError(err)
+		assert.NotNil(group.ID)
 	})
 
-	if assert.NoError(t, err) {
-		assert.NotNil(t, group.ID)
-	}
+	t.Run("create group with invalid members", func(t *testing.T) {
+		var p writeGroupParams
+		require.NoError(copier.Copy(&p, &params))
+		p.Members = append(p.Members, mustNewUUIDV4(t))
+		_, err := createGroup(r.db, params)
+		require.NoError(err)
+	})
 }
