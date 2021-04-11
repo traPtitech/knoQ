dc82909ed438e694724073aa89f8ca159c3e0250
fuji <haya10.88ha@gmail.com>
2021-04-11 21:17:55 +0900

    impl users

--- router/users.go
+++ router/users.go
@@ -1,56 +1,47 @@
 package router
 
 import (
-	"encoding/json"
 	"errors"
-	"io/ioutil"
 	"net/http"
-	"net/url"
-	"path"
+	"strconv"
 
-	"github.com/traPtitech/knoQ/router/service"
+	"github.com/traPtitech/knoQ/domain"
+	"github.com/traPtitech/knoQ/presentation"
+	"github.com/traPtitech/knoQ/usecase/production"
 
 	"github.com/labstack/echo/v4"
-	traQv3 "github.com/traPtitech/traQ/router/v3"
-	traQrandom "github.com/traPtitech/traQ/utils/random"
 )
 
 // HandleGetUserMe ヘッダー情報からuser情報を取得
 // 認証状態を確認
 func (h *Handlers) HandleGetUserMe(c echo.Context) error {
-	token, _ := getRequestUserToken(c)
-	userID, _ := getRequestUserID(c)
-
-	user, err := h.Dao.GetUser(token, userID)
+	user, err := h.repo.GetUserMe(getConinfo(c))
 	if err != nil {
-		if err.Error() == http.StatusText(http.StatusUnauthorized) {
-			h.Repo.ReplaceToken(userID, "")
+		if errors.Is(domain.ErrInvalidToken, err) {
 			return forbidden(err, message("token is invalid."), needAuthorization(true))
 		}
 		return judgeErrorResponse(err)
 	}
-	return c.JSON(http.StatusOK, service.FormatUserRes(user))
+	return c.JSON(http.StatusOK, presentation.ConvdomainUserToUserRes(*user))
 }
 
 // HandleGetUsers ユーザーすべてを取得
 func (h *Handlers) HandleGetUsers(c echo.Context) error {
-	token, _ := getRequestUserToken(c)
-	userID, _ := getRequestUserID(c)
+	includeSuspend, _ := strconv.ParseBool(c.QueryParam("include-suspended"))
 
-	users, err := h.Dao.GetAllUsers(token)
+	users, err := h.repo.GetAllUsers(includeSuspend, getConinfo(c))
 	if err != nil {
-		if err.Error() == http.StatusText(http.StatusUnauthorized) {
-			h.Repo.ReplaceToken(userID, "")
+		if errors.Is(domain.ErrInvalidToken, err) {
 			return forbidden(err, message("token is invalid."), needAuthorization(true))
 		}
 		return judgeErrorResponse(err)
 	}
 
-	return c.JSON(http.StatusOK, service.FormatUsersRes(users))
+	return c.JSON(http.StatusOK, presentation.ConvSPdomainUserToSUserRes(users))
 }
+
 func (h *Handlers) HandleGetiCal(c echo.Context) error {
-	userID, _ := getRequestUserID(c)
-	secret, err := h.Dao.Repo.GetiCalSecret(userID)
+	secret, err := h.repo.GetMyiCalSecret(getConinfo(c))
 	if err != nil {
 		return judgeErrorResponse(err)
 	}
@@ -62,9 +53,8 @@ func (h *Handlers) HandleGetiCal(c echo.Context) error {
 }
 
 func (h *Handlers) HandleUpdateiCal(c echo.Context) error {
-	userID, _ := getRequestUserID(c)
-	secret := traQrandom.SecureAlphaNumeric(16)
-	if err := h.Dao.Repo.ReplaceiCalSecret(userID, secret); err != nil {
+	secret, err := h.repo.ReNewMyiCalSecret(getConinfo(c))
+	if err != nil {
 		return judgeErrorResponse(err)
 	}
 	return c.JSON(http.StatusOK, struct {
@@ -78,52 +68,14 @@ func (h *Handlers) HandleUpdateiCal(c echo.Context) error {
 // 停止されているユーザーの`token`を削除して、
 // 活動中のユーザーを追加する(userIDをDBに保存)
 func (h *Handlers) HandleSyncUser(c echo.Context) error {
-	token, _ := getRequestUserToken(c)
-
-	// TODO fix with repository v4
-	allUsers, err := getTraQAllUsers(token)
+	repo, ok := h.repo.(*production.Repository)
+	if !ok {
+		return internalServerError(errors.New("not implemented"))
+	}
+	err := repo.SyncUsers(getConinfo(c))
 	if err != nil {
-		return internalServerError(err)
+		return judgeErrorResponse(err)
 	}
-	for _, user := range allUsers {
-		if user.State == 0 {
-			h.Repo.ReplaceToken(user.ID, "")
-		} else if user.State == 1 {
-			h.Repo.SaveUser(user.ID, false, true)
-		}
-	}
+
 	return c.NoContent(http.StatusCreated)
-}
-
-func getTraQAllUsers(token string) ([]traQv3.User, error) {
-	u, err := url.Parse("https://q.trap.jp/api/v3")
-	if err != nil {
-		return nil, err
-	}
-	u.Path = path.Join(u.Path, "users")
-	query := u.Query()
-	query.Set("include-suspended", "1")
-	u.RawQuery = query.Encode()
-
-	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("Authorization", "Bearer "+token)
-
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	if res.StatusCode >= 400 {
-		return nil, errors.New(http.StatusText(res.StatusCode))
-	}
-
-	data, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return nil, err
-	}
-	users := make([]traQv3.User, 0)
-	err = json.Unmarshal(data, &users)
-	return users, err
 }
