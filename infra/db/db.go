62257e4da7c460f3fc2e17026986308dbbd0e81d
fuji <haya10.88ha@gmail.com>
2021-05-04 22:28:36 +0900

    migrate user

--- infra/db/db.go
+++ infra/db/db.go
@@ -37,7 +37,7 @@ func (repo *GormRepository) Setup(host, user, password, database, key string) er
 		DefaultStringSize:         256,   // default size for string fields
 		DisableDatetimePrecision:  true,  // disable datetime precision, which not supported before MySQL 5.6
 		DontSupportRenameIndex:    true,  // drop & create when rename index, rename index not supported before MySQL 5.7, MariaDB
-		DontSupportRenameColumn:   true,  // `change` when rename column, rename column not supported before MySQL 8, MariaDB
+		DontSupportRenameColumn:   false, // `change` when rename column, rename column not supported before MySQL 8, MariaDB
 		SkipInitializeWithVersion: false, // auto configure based on currently MySQL version
 	}), &gorm.Config{})
 	if err != nil {
