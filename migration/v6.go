ae9990b7275b57497bb77ada775e40e867f42c9d
fuji <haya10.88ha@gmail.com>
2021-05-04 22:28:26 +0900

    fix

---
+++ migration/v6.go
@@ -0,0 +1,31 @@
+package migration
+
+import (
+	gormigrate "github.com/go-gormigrate/gormigrate/v2"
+	"gorm.io/gorm"
+)
+
+// type v6newRoom struct {
+// Verified bool
+// }
+
+// func (*v6newRoom) TableName() string {
+// return "rooms"
+// }
+
+// type v6currentRoom struct {
+// Public bool
+// }
+
+// func (*v6currentRoom) TableName() string {
+// return "rooms"
+// }
+
+func v6() *gormigrate.Migration {
+	return &gormigrate.Migration{
+		ID: "6",
+		Migrate: func(db *gorm.DB) error {
+			return db.Migrator().RenameColumn("rooms", "public", "verifed")
+		},
+	}
+}
