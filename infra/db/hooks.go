1ffb11afa72e530a301795d62945b8ed29b19e11
ras0q <ras0q@users.noreply.github.com>
2023-05-19 14:53:18 +0900

    :recycle: if err != nil -> errors.Is()

--- infra/db/hooks.go
+++ infra/db/hooks.go
@@ -17,22 +17,23 @@ func (e *Event) BeforeSave(tx *gorm.DB) (err error) {
 	}
 
 	if e.RoomID == uuid.Nil {
-		if e.Room.Place != "" {
-			err := tx.
-				Where(&Room{Place: e.Room.Place, TimeStart: e.TimeStart, TimeEnd: e.TimeEnd}).
-				First(&e.Room).
-				Error
+		if e.Room.Place == "" {
+			return NewValueError(ErrRoomUndefined, "roomID", "place")
+		}
+
+		if err := tx.
+			Where(&Room{Place: e.Room.Place, TimeStart: e.TimeStart, TimeEnd: e.TimeEnd}).
+			First(&e.Room).
+			Error; err != nil {
 			if errors.Is(err, gorm.ErrRecordNotFound) {
 				e.Room.Verified = false
 				e.Room.TimeStart = e.TimeStart
 				e.Room.TimeEnd = e.TimeEnd
 				e.Room.CreatedByRefer = e.CreatedByRefer
 				e.Room.Admins = ConvSEventAdminToSRoomAdmin(e.Admins)
-			} else if err != nil {
+			} else {
 				return err
 			}
-		} else {
-			return NewValueError(ErrRoomUndefined, "roomID", "place")
 		}
 	}
 
