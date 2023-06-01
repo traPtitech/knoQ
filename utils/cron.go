28fb6b697194bee2bb729e8362b87b8cf50c5632
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2023-06-01 18:01:27 +0900

    fix: 授業時間外に予約時間が開始・終了する場合に対応

--- utils/cron.go
+++ utils/cron.go
@@ -29,9 +29,9 @@ func InitPostEventToTraQ(repo *db.GormRepository, secret, channelID, webhookID, 
 }
 
 type timeTable struct {
-	name  string
-	start time.Time
-	end   time.Time
+	name           string
+	start          time.Time
+	displayDefault bool
 }
 
 func setTimeFromString(t time.Time, str string) time.Time {
@@ -55,29 +55,36 @@ func createMessage(t time.Time, rooms []*domain.Room, events []*db.Event, origin
 
 	timeTables := []timeTable{
 		{
-			name:  "1-2",
-			start: setTimeFromString(t, "08:50:00"),
-			end:   setTimeFromString(t, "10:30:00"),
+			name:  ":sunny: 朝",
+			start: setTimeFromString(t, "00:00:00"),
+		},
+		{
+			name:           "1-2",
+			start:          setTimeFromString(t, "08:50:00"),
+			displayDefault: true,
 		}, {
-			name:  "3-4",
-			start: setTimeFromString(t, "10:45:00"),
-			end:   setTimeFromString(t, "12:25:00"),
+			name:           "3-4",
+			start:          setTimeFromString(t, "10:45:00"),
+			displayDefault: true,
 		}, {
-			name:  "昼",
-			start: setTimeFromString(t, "12:25:00"),
-			end:   setTimeFromString(t, "13:45:00"),
+			name:           "昼",
+			start:          setTimeFromString(t, "12:25:00"),
+			displayDefault: true,
 		}, {
-			name:  "5-6",
-			start: setTimeFromString(t, "13:45:00"),
-			end:   setTimeFromString(t, "15:25:00"),
+			name:           "5-6",
+			start:          setTimeFromString(t, "13:45:00"),
+			displayDefault: true,
 		}, {
-			name:  "7-8",
-			start: setTimeFromString(t, "15:40:00"),
-			end:   setTimeFromString(t, "17:20:00"),
+			name:           "7-8",
+			start:          setTimeFromString(t, "15:40:00"),
+			displayDefault: true,
 		}, {
-			name:  "9-10",
-			start: setTimeFromString(t, "17:30:00"),
-			end:   setTimeFromString(t, "19:10:00"),
+			name:           "9-10",
+			start:          setTimeFromString(t, "17:30:00"),
+			displayDefault: true,
+		}, {
+			name:  ":crescent_moon: 夜",
+			start: setTimeFromString(t, "19:10:00"),
 		},
 	}
 
@@ -108,9 +115,17 @@ func createMessage(t time.Time, rooms []*domain.Room, events []*db.Event, origin
 				}
 
 				for i, row := range timeTables {
+					var rowNextStart time.Time
+
+					if i != len(timeTables)-1 {
+						rowNextStart = timeTables[i+1].start
+					} else {
+						rowNextStart = setTimeFromString(t, "23:59:59")
+					}
+
 					if timeLessThanOrEqual(room.TimeStart, row.start) { // room.TimeStart <= row.start
 						switch {
-						case timeLessThanOrEqual(row.end, room.TimeEnd): // row.end <= room.TimeEnd
+						case timeLessThanOrEqual(rowNextStart, room.TimeEnd): // row.end <= room.TimeEnd
 							roomAvailable[i][room.Place] = ":white_check_mark:"
 
 						case room.TimeEnd.After(row.start): // row.start < room.TimeEnd < row.end
@@ -122,7 +137,7 @@ func createMessage(t time.Time, rooms []*domain.Room, events []*db.Event, origin
 							}
 						}
 					} else { // row.start < room.TimeStart
-						if timeLessThanOrEqual(row.end, room.TimeStart) { // row.end <= room.TimeStart
+						if timeLessThanOrEqual(rowNextStart, room.TimeStart) { // row.end <= room.TimeStart
 							if _, ok := roomAvailable[i][room.Place]; !ok {
 								roomAvailable[i][room.Place] = ":regional_indicator_null:"
 							}
@@ -130,7 +145,7 @@ func createMessage(t time.Time, rooms []*domain.Room, events []*db.Event, origin
 						}
 						// row.start < room.TimeStart < row.end
 
-						if timeLessThanOrEqual(row.end, room.TimeEnd) { // row.end <= room.TimeEnd
+						if timeLessThanOrEqual(rowNextStart, room.TimeEnd) { // row.end <= room.TimeEnd
 							roomAvailable[i][room.Place] = fmt.Sprintf("%s -", room.TimeStart.Format("15:04"))
 							continue
 						}
@@ -143,6 +158,17 @@ func createMessage(t time.Time, rooms []*domain.Room, events []*db.Event, origin
 
 			roomMessage = fmt.Sprintf("| time | %s |\n| :---: | %s \n", strings.Join(verifiedRoomNames, " | "), strings.Repeat(" :---: |", len(verifiedRoomNames)))
 			for i, row := range timeTables {
+				f := row.displayDefault
+				for _, col := range verifiedRoomNames {
+					if roomAvailable[i][col] != ":regional_indicator_null:" {
+						f = true
+					}
+				}
+
+				if !f {
+					continue
+				}
+
 				roomMessage += fmt.Sprintf("| %s |", row.name)
 
 				for _, col := range verifiedRoomNames {
