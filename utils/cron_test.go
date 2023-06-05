68300d764d8fba116e9c4a57a2aeb73df7622bf9
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2023-06-05 18:39:53 +0900

    Revert "test: cron_test"
    This reverts commit 561335f0c62fab09886399ae6eeb4f29bc626162.

--- utils/cron_test.go
+++ utils/cron_test.go
@@ -1,20 +1,13 @@
 package utils
 
-import (
-	"github.com/traPtitech/knoQ/infra/db"
+//func Test_initPostEventToTraQ(t *testing.T) {
+//db, _ := repo.SetupDatabase()
+//gr := &repo.GormRepository{
+//DB: db,
+//}
 
-	"os"
-	"testing"
-)
+//job := InitPostEventToTraQ(gr, "", "", "", "localhost:4000")
 
-func Test_initPostEventToTraQ(t *testing.T) {
-	gormRepo := db.GormRepository{}
-	err := gormRepo.Setup(os.Getenv("MARIADB_HOSTNAME"), os.Getenv("MARIADB_USERNAME"),
-		os.Getenv("MARIADB_PASSWORD"), os.Getenv("MARIADB_DATABASE"), os.Getenv("TOKEN_KEY"), "silent")
-	if err != nil {
-		panic(err)
-	}
-
-	InitPostEventToTraQ(&gormRepo, os.Getenv("WEBHOOK_SECRET"),
-		os.Getenv("ACTIVITY_CHANNEL_ID"), os.Getenv("WEBHOOK_ID"), os.Getenv("ORIGIN"))()
-}
+//scheduler.Every(2).Seconds().Run(job)
+//time.Sleep(time.Second * 2)
+//}
