ffbc92c7d647c6274ca906f36a07fc60141e7521
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2023-06-25 07:29:32 +0900

    test: refactor

--- utils/cron_test.go
+++ utils/cron_test.go
@@ -9,175 +9,166 @@ import (
 )
 
 func Test_makeRoomAvailableByTimeTable(t *testing.T) {
-
-	date := time.Now()
-
+	today := time.Now()
 	tt := []timeTable{
-		{":sunny:", setTimeFromString(date, "00:00:00"), false},
-		{"1-2", setTimeFromString(date, "08:50:00"), true},
-		{"3-4", setTimeFromString(date, "10:45:00"), true},
-		{"昼", setTimeFromString(date, "12:25:00"), true},
-		{"5-6", setTimeFromString(date, "13:45:00"), true},
-		{"7-8", setTimeFromString(date, "15:40:00"), true},
-		{"9-10", setTimeFromString(date, "17:30:00"), true},
-		{":crescent_moon:", setTimeFromString(date, "19:10:00"), false},
+		{":sunny:", setTimeFromString(today, "00:00:00"), false},
+		{"1-2", setTimeFromString(today, "08:50:00"), true},
+		{"3-4", setTimeFromString(today, "10:45:00"), true},
+		{"昼", setTimeFromString(today, "12:25:00"), true},
+		{"5-6", setTimeFromString(today, "13:45:00"), true},
+		{"7-8", setTimeFromString(today, "15:40:00"), true},
+		{"9-10", setTimeFromString(today, "17:30:00"), true},
+		{":crescent_moon:", setTimeFromString(today, "19:10:00"), false},
 	}
 
-	stampOk := ":white_check_mark:"
-	stampNo := ":regional_indicator_null:"
-	tests := []struct {
-		name string
+	stampAvailable := ":white_check_mark:"
+	stampNotAvailable := ":regional_indicator_null:"
+	tests := map[string]struct {
 		room []*domain.Room
 		want []map[string]string
 	}{
-		{
-			name: "1-10",
+		"1-10": {
 			room: []*domain.Room{
 				{
 					Place:     "traP-001",
 					Verified:  true,
-					TimeStart: setTimeFromString(date, "08:50:00"),
-					TimeEnd:   setTimeFromString(date, "19:10:00"),
+					TimeStart: setTimeFromString(today, "08:50:00"),
+					TimeEnd:   setTimeFromString(today, "19:10:00"),
 				},
 			},
 			want: []map[string]string{
-				{"traP-001": stampNo},
-				{"traP-001": stampOk},
-				{"traP-001": stampOk},
-				{"traP-001": stampOk},
-				{"traP-001": stampOk},
-				{"traP-001": stampOk},
-				{"traP-001": stampOk},
-				{"traP-001": stampNo},
+				{"traP-001": stampNotAvailable},
+				{"traP-001": stampAvailable},
+				{"traP-001": stampAvailable},
+				{"traP-001": stampAvailable},
+				{"traP-001": stampAvailable},
+				{"traP-001": stampAvailable},
+				{"traP-001": stampAvailable},
+				{"traP-001": stampNotAvailable},
 			},
 		},
-		{
-			name: "cut unverified",
+		"cut unverified": {
 			room: []*domain.Room{
 				{
 					Place:     "traP-001",
 					Verified:  true,
-					TimeStart: setTimeFromString(date, "08:50:00"),
-					TimeEnd:   setTimeFromString(date, "19:10:00"),
+					TimeStart: setTimeFromString(today, "08:50:00"),
+					TimeEnd:   setTimeFromString(today, "19:10:00"),
 				},
 				{
 					Place:     "unverified",
 					Verified:  false,
-					TimeStart: setTimeFromString(date, "08:50:00"),
-					TimeEnd:   setTimeFromString(date, "19:10:00"),
+					TimeStart: setTimeFromString(today, "08:50:00"),
+					TimeEnd:   setTimeFromString(today, "19:10:00"),
 				},
 			},
 			want: []map[string]string{
-				{"traP-001": stampNo},
-				{"traP-001": stampOk},
-				{"traP-001": stampOk},
-				{"traP-001": stampOk},
-				{"traP-001": stampOk},
-				{"traP-001": stampOk},
-				{"traP-001": stampOk},
-				{"traP-001": stampNo},
+				{"traP-001": stampNotAvailable},
+				{"traP-001": stampAvailable},
+				{"traP-001": stampAvailable},
+				{"traP-001": stampAvailable},
+				{"traP-001": stampAvailable},
+				{"traP-001": stampAvailable},
+				{"traP-001": stampAvailable},
+				{"traP-001": stampNotAvailable},
 			},
 		},
-		{
-			name: "24/7",
+		"24/7": {
 			room: []*domain.Room{
 				{
 					Place:     "traP-001",
 					Verified:  true,
-					TimeStart: setTimeFromString(date, "00:00:00"),
-					TimeEnd:   setTimeFromString(date, "23:59:59"),
+					TimeStart: setTimeFromString(today, "00:00:00"),
+					TimeEnd:   setTimeFromString(today, "23:59:59"),
 				},
 			},
 			want: []map[string]string{
-				{"traP-001": stampOk},
-				{"traP-001": stampOk},
-				{"traP-001": stampOk},
-				{"traP-001": stampOk},
-				{"traP-001": stampOk},
-				{"traP-001": stampOk},
-				{"traP-001": stampOk},
-				{"traP-001": stampOk},
+				{"traP-001": stampAvailable},
+				{"traP-001": stampAvailable},
+				{"traP-001": stampAvailable},
+				{"traP-001": stampAvailable},
+				{"traP-001": stampAvailable},
+				{"traP-001": stampAvailable},
+				{"traP-001": stampAvailable},
+				{"traP-001": stampAvailable},
 			},
 		},
-		{
-			name: "5-10 and 3-10 except Lunch",
+		"5-10 and 3-10 except Lunch": {
 			room: []*domain.Room{
 				{
 					Place:     "traP-001",
 					Verified:  true,
-					TimeStart: setTimeFromString(date, "12:25:00"),
-					TimeEnd:   setTimeFromString(date, "19:10:00"),
+					TimeStart: setTimeFromString(today, "12:25:00"),
+					TimeEnd:   setTimeFromString(today, "19:10:00"),
 				},
 				{
 					Place:     "traP-002",
 					Verified:  true,
-					TimeStart: setTimeFromString(date, "10:45:00"),
-					TimeEnd:   setTimeFromString(date, "12:25:00"),
+					TimeStart: setTimeFromString(today, "10:45:00"),
+					TimeEnd:   setTimeFromString(today, "12:25:00"),
 				},
 				{
 					Place:     "traP-002",
 					Verified:  true,
-					TimeStart: setTimeFromString(date, "13:45:00"),
-					TimeEnd:   setTimeFromString(date, "19:10:00"),
+					TimeStart: setTimeFromString(today, "13:45:00"),
+					TimeEnd:   setTimeFromString(today, "19:10:00"),
 				},
 			},
 			want: []map[string]string{
-				{"traP-001": stampNo, "traP-002": stampNo},
-				{"traP-001": stampNo, "traP-002": stampNo},
-				{"traP-001": stampNo, "traP-002": stampOk},
-				{"traP-001": stampOk, "traP-002": stampNo},
-				{"traP-001": stampOk, "traP-002": stampOk},
-				{"traP-001": stampOk, "traP-002": stampOk},
-				{"traP-001": stampOk, "traP-002": stampOk},
-				{"traP-001": stampNo, "traP-002": stampNo},
+				{"traP-001": stampNotAvailable, "traP-002": stampNotAvailable},
+				{"traP-001": stampNotAvailable, "traP-002": stampNotAvailable},
+				{"traP-001": stampNotAvailable, "traP-002": stampAvailable},
+				{"traP-001": stampAvailable, "traP-002": stampNotAvailable},
+				{"traP-001": stampAvailable, "traP-002": stampAvailable},
+				{"traP-001": stampAvailable, "traP-002": stampAvailable},
+				{"traP-001": stampAvailable, "traP-002": stampAvailable},
+				{"traP-001": stampNotAvailable, "traP-002": stampNotAvailable},
 			},
 		},
-		{
-			name: "10:00-18:00",
+		"10:00-18:00": {
 			room: []*domain.Room{
 				{
 					Place:     "traP-001",
 					Verified:  true,
-					TimeStart: setTimeFromString(date, "10:00:00"),
-					TimeEnd:   setTimeFromString(date, "18:00:00"),
+					TimeStart: setTimeFromString(today, "10:00:00"),
+					TimeEnd:   setTimeFromString(today, "18:00:00"),
 				},
 			},
 			want: []map[string]string{
-				{"traP-001": stampNo},
+				{"traP-001": stampNotAvailable},
 				{"traP-001": "10:00 -"},
-				{"traP-001": stampOk},
-				{"traP-001": stampOk},
-				{"traP-001": stampOk},
-				{"traP-001": stampOk},
+				{"traP-001": stampAvailable},
+				{"traP-001": stampAvailable},
+				{"traP-001": stampAvailable},
+				{"traP-001": stampAvailable},
 				{"traP-001": "- 18:00"},
-				{"traP-001": stampNo},
+				{"traP-001": stampNotAvailable},
 			},
 		},
-		{
-			name: "15:00-15:01",
+		"15:00-15:01": {
 			room: []*domain.Room{
 				{
 					Place:     "traP-001",
 					Verified:  true,
-					TimeStart: setTimeFromString(date, "15:00:00"),
-					TimeEnd:   setTimeFromString(date, "15:01:00"),
+					TimeStart: setTimeFromString(today, "15:00:00"),
+					TimeEnd:   setTimeFromString(today, "15:01:00"),
 				},
 			},
 			want: []map[string]string{
-				{"traP-001": stampNo},
-				{"traP-001": stampNo},
-				{"traP-001": stampNo},
-				{"traP-001": stampNo},
+				{"traP-001": stampNotAvailable},
+				{"traP-001": stampNotAvailable},
+				{"traP-001": stampNotAvailable},
+				{"traP-001": stampNotAvailable},
 				{"traP-001": "15:00 - 15:01"},
-				{"traP-001": stampNo},
-				{"traP-001": stampNo},
-				{"traP-001": stampNo},
+				{"traP-001": stampNotAvailable},
+				{"traP-001": stampNotAvailable},
+				{"traP-001": stampNotAvailable},
 			},
 		},
 	}
-	for _, te := range tests {
-		t.Run(te.name, func(t *testing.T) {
-			got := makeRoomAvailableByTimeTable(te.room, tt, time.Now())
+	for name, te := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := makeRoomAvailableByTimeTable(te.room, tt, today)
 
 			if !reflect.DeepEqual(got, te.want) {
 				if !(len(got) == 0 && len(te.want) == 0) {
