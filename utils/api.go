2566f5fc315c5ab58f829c0bcc5d4ece79f86f45
Nzt3 <nzt3pro@gmail.com>
2023-09-24 18:50:56 +0900

    disable previous code

--- utils/api.go
+++ utils/api.go
@@ -7,12 +7,12 @@ import (
 	"encoding/hex"
 	"errors"
 	"net/http"
-	"net/url"
-	"path"
-	"strconv"
-	"strings"
+	// "net/url"
+	// "path"
+	// "strconv"
+	// "strings"
 
-	"github.com/labstack/echo/v4"
+	// "github.com/labstack/echo/v4"
 	"github.com/traPtitech/go-traq"
 )
 
@@ -20,54 +20,46 @@ const baseURL = "https://q.trap.jp/api/v3"
 
 // RequestWebhook q.trap/jp にメッセージを送信します。
 func RequestWebhook(message, secret, channelID, webhookID string, embed int) error {
-	u, err := url.Parse(baseURL + "/webhooks")
-	if err != nil {
-		return err
-	}
-	u.Path = path.Join(u.Path, webhookID)
-	query := u.Query()
-	query.Set("embed", strconv.Itoa(embed))
-	u.RawQuery = query.Encode()
+	// u, err := url.Parse(baseURL + "/webhooks")
+	// if err != nil {
+	// 	return err
+	// }
+	// u.Path = path.Join(u.Path, webhookID)
+	// query := u.Query()
+	// query.Set("embed", strconv.Itoa(embed))
+	// u.RawQuery = query.Encode()
 
-	req, err := http.NewRequest(http.MethodPost, u.String(), strings.NewReader(message))
-	if err != nil {
-		return err
-	}
-	req.Header.Set(echo.HeaderContentType, echo.MIMETextPlain)
-	req.Header.Set("X-TRAQ-Signature", calcSignature(message, secret))
-	if channelID != "" {
-		req.Header.Set("X-TRAQ-Channel-Id", channelID)
-	}
+	// req, err := http.NewRequest(http.MethodPost, u.String(), strings.NewReader(message))
+	// if err != nil {
+	// 	return err
+	// }
+	// req.Header.Set(echo.HeaderContentType, echo.MIMETextPlain)
+	// req.Header.Set("X-TRAQ-Signature", calcSignature(message, secret))
+	// if channelID != "" {
+	// 	req.Header.Set("X-TRAQ-Channel-Id", channelID)
+	// }
 
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
+	// res, err := http.DefaultClient.Do(req)
+	// if err != nil {
+	// 	return err
+	// }
+	// if res.StatusCode >= 400 {
+	// 	return errors.New(http.StatusText(res.StatusCode))
+	// }
+
+	// ここから go-traq 書き換え
+	xTRAQSignature := calcSignature(message, secret)
+	configuration := traq.NewConfiguration()
+  apiClient := traq.NewAPIClient(configuration)
+
+	res, err := apiClient.WebhookApi.PostWebhook(context.Background(),webhookID).XTRAQChannelId(channelID).XTRAQSignature(xTRAQSignature).Embed(int32(embed)).Body(message).Execute()
+	if err != nil{
 		return err
 	}
 	if res.StatusCode >= 400 {
 		return errors.New(http.StatusText(res.StatusCode))
 	}
 
-	// ここから go-traq 書き換え
-	xTRAQSignature := calcSignature(message, secret)
-	configuration := traq.NewConfiguration()
-  apiClient := traq.NewAPIClient(configuration)
-	if channelID != "" {
-		res, err := apiClient.WebhookApi.PostWebhook(context.Background(),webhookID).XTRAQChannelId(channelID).XTRAQSignature(xTRAQSignature).Embed(int32(embed)).Body(message).Execute()
-		if err != nil{
-			return err
-		}
-		if res.StatusCode >= 400 {
-			return errors.New(http.StatusText(res.StatusCode))
-		}
-	}else{
-		res, err := apiClient.WebhookApi.PostWebhook(context.Background(),webhookID).XTRAQSignature(xTRAQSignature).Embed(int32(embed)).Body(message).Execute()
-		if err != nil{
-			return err
-		}
-		if res.StatusCode >= 400 {
-			return errors.New(http.StatusText(res.StatusCode))
-		}
-	}
 	// ここまで go-traq 書き換え
 
 	return nil
