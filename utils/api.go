862b47af4f882e94be3e8ad5248e19a2c0fd7507
Nzt3 <nzt3pro@gmail.com>
2023-10-26 21:18:06 +0900

    delete unused variable

--- utils/api.go
+++ utils/api.go
@@ -7,60 +7,25 @@ import (
 	"encoding/hex"
 	"errors"
 	"net/http"
-	// "net/url"
-	// "path"
-	// "strconv"
-	// "strings"
 
-	// "github.com/labstack/echo/v4"
 	"github.com/traPtitech/go-traq"
 )
 
-const baseURL = "https://q.trap.jp/api/v3"
+
 
 // RequestWebhook q.trap/jp にメッセージを送信します。
 func RequestWebhook(message, secret, channelID, webhookID string, embed int) error {
-	// u, err := url.Parse(baseURL + "/webhooks")
-	// if err != nil {
-	// 	return err
-	// }
-	// u.Path = path.Join(u.Path, webhookID)
-	// query := u.Query()
-	// query.Set("embed", strconv.Itoa(embed))
-	// u.RawQuery = query.Encode()
-
-	// req, err := http.NewRequest(http.MethodPost, u.String(), strings.NewReader(message))
-	// if err != nil {
-	// 	return err
-	// }
-	// req.Header.Set(echo.HeaderContentType, echo.MIMETextPlain)
-	// req.Header.Set("X-TRAQ-Signature", calcSignature(message, secret))
-	// if channelID != "" {
-	// 	req.Header.Set("X-TRAQ-Channel-Id", channelID)
-	// }
-
-	// res, err := http.DefaultClient.Do(req)
-	// if err != nil {
-	// 	return err
-	// }
-	// if res.StatusCode >= 400 {
-	// 	return errors.New(http.StatusText(res.StatusCode))
-	// }
-
-	// ここから go-traq 書き換え
 	xTRAQSignature := calcSignature(message, secret)
 	configuration := traq.NewConfiguration()
-  apiClient := traq.NewAPIClient(configuration)
+	apiClient := traq.NewAPIClient(configuration)
 
-	res, err := apiClient.WebhookApi.PostWebhook(context.Background(),webhookID).XTRAQChannelId(channelID).XTRAQSignature(xTRAQSignature).Embed(int32(embed)).Body(message).Execute()
-	if err != nil{
+	res, err := apiClient.WebhookApi.PostWebhook(context.Background(), webhookID).XTRAQChannelId(channelID).XTRAQSignature(xTRAQSignature).Embed(int32(embed)).Body(message).Execute()
+	if err != nil {
 		return err
 	}
 	if res.StatusCode >= 400 {
 		return errors.New(http.StatusText(res.StatusCode))
 	}
-
-	// ここまで go-traq 書き換え
 
 	return nil
 }
