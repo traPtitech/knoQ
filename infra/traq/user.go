2f0d59990f3426a72c9ddf0c99bbe8ec7177b6de
Nzt3 <nzt3pro@gmail.com>
2023-10-26 23:05:11 +0900

    expand converter func UserDetail,MyUserDetail->User

--- infra/traq/user.go
+++ infra/traq/user.go
@@ -14,7 +14,14 @@ func (repo *TraQRepository) GetUser(token *oauth2.Token, userID uuid.UUID) (*tra
 	if err != nil {
 		return nil, err
 	}
-	user := convertUserdetailToUser(userDetail)
+	user := new(traq.User)
+	user.Id = userDetail.Id
+	user.Name = userDetail.Name
+	user.DisplayName = userDetail.DisplayName
+	user.IconFileId = userDetail.IconFileId
+	user.Bot = userDetail.Bot
+	user.State = userDetail.State
+	user.UpdatedAt = userDetail.UpdatedAt
 	return user, err
 }
 
@@ -31,37 +38,21 @@ func (repo *TraQRepository) GetUsers(token *oauth2.Token, includeSuspended bool)
 func (repo *TraQRepository) GetUserMe(token *oauth2.Token) (*traq.User, error) {
 	apiClient := MakeApiClient(token)
 
-	data, _, err := apiClient.MeApi.GetMe(context.Background()).Execute()
+	userDetail, _, err := apiClient.MeApi.GetMe(context.Background()).Execute()
 	if err != nil {
 		return nil, err
 	}
-	user := convertMyUserdetailToUser(data)
+	user := new(traq.User)
+	user.Id = userDetail.Id
+	user.Name = userDetail.Name
+	user.DisplayName = userDetail.DisplayName
+	user.IconFileId = userDetail.IconFileId
+	user.Bot = userDetail.Bot
+	user.State = userDetail.State
+	user.UpdatedAt = userDetail.UpdatedAt
 	return user, err
 }
 
-func convertMyUserdetailToUser(userdetail *traq.MyUserDetail) *traq.User {
-	user := new(traq.User)
-	user.Id = userdetail.Id
-	user.Name = userdetail.Name
-	user.DisplayName = userdetail.DisplayName
-	user.IconFileId = userdetail.IconFileId
-	user.Bot = userdetail.Bot
-	user.State = userdetail.State
-	user.UpdatedAt = userdetail.UpdatedAt
-	return user
-}
-
-func convertUserdetailToUser(userdetail *traq.UserDetail) *traq.User {
-	user := new(traq.User)
-	user.Id = userdetail.Id
-	user.Name = userdetail.Name
-	user.DisplayName = userdetail.DisplayName
-	user.IconFileId = userdetail.IconFileId
-	user.Bot = userdetail.Bot
-	user.State = userdetail.State
-	user.UpdatedAt = userdetail.UpdatedAt
-	return user
-}
 func convertUsersToUsers(users []traq.User) []*traq.User {
 	new_users := make([]*traq.User, len(users))
 	for i, _user := range users {
