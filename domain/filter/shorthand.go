e80bc38dc79991e0ebae26afe7382406fd2f9456
iChemy <controlyuu.dev@gmail.com>
2024-06-24 15:18:31 +0900

    :art: add zero time handling

--- domain/filter/shorthand.go
+++ domain/filter/shorthand.go
@@ -99,11 +99,29 @@ func AddAnd(lhs, rhs Expr) Expr {
 // * 期間中に始まる (time_start >= since AND time_start < until)
 // * 期間中に終わる (time_end >= since AND time_end < until)
 // * 期間より前に始まり期間より後に終わる (time_start < since AND time_end >= until)
+//
+// ただしsinceがゼロのとき time_start <= until
+// untilがゼロのとき since <= time_end
+// 双方がゼロのき nil を返す
 func FilterDuration(since, until time.Time) (Expr, error) {
-	if !since.IsZero() && !until.IsZero() {
-		if since.After(until) {
-			return nil, errors.New("invalid time range")
-		}
+	if since.IsZero() && until.IsZero() {
+		return nil, nil
+	} else if since.IsZero() {
+		return &CmpExpr{
+			Attr:     AttrTimeStart,
+			Relation: LessEq,
+			Value:    until,
+		}, nil
+	} else if until.IsZero() {
+		return &CmpExpr{
+			Attr:     AttrTimeEnd,
+			Relation: GreterEq,
+			Value:    since,
+		}, nil
+	}
+
+	if since.After(until) {
+		return nil, errors.New("invalid time range")
 	}
 
 	// 期間中に始まる
@@ -136,21 +154,19 @@ func FilterDuration(since, until time.Time) (Expr, error) {
 		},
 	}
 
-	startBeforeMin := &CmpExpr{
-		Attr:     AttrTimeStart,
-		Relation: Less,
-		Value:    since,
-	}
-	endAfterMax := &CmpExpr{
-		Attr:     AttrTimeEnd,
-		Relation: GreterEq,
-		Value:    until,
-	}
 	// 期間より前に始まり期間より後に終わる
 	throughout := &LogicOpExpr{
 		LogicOp: And,
-		Lhs:     startBeforeMin,
-		Rhs:     endAfterMax,
+		Lhs: &CmpExpr{
+			Attr:     AttrTimeStart,
+			Relation: Less,
+			Value:    since,
+		},
+		Rhs: &CmpExpr{
+			Attr:     AttrTimeEnd,
+			Relation: GreterEq,
+			Value:    until,
+		},
 	}
 
 	return &LogicOpExpr{
