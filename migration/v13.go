b3fc75b47114771b3100ca7b5e0642e9cb238662
pasmophobia <l@leonmatsuistroemsvik.com>
2024-07-06 23:24:37 +0900

    :bug: fixed sql statement in v13.go

--- migration/v13.go
+++ migration/v13.go
@@ -9,7 +9,22 @@ func v13() *gormigrate.Migration {
 	return &gormigrate.Migration{
 		ID: "13",
 		Migrate: func(db *gorm.DB) error {
-			return db.Exec("ALTER TABLE events DROP CONSTRAINT UNIQUE(name, time_start, time_end)").Error
+			return db.Exec(
+				`
+SET @sql = (
+	SELECT IF(
+		COUNT(*) > 0,
+		'ALTER TABLE events DROP CONSTRAINT idx_name_time_start_time_end',
+		'SELECT "Constraint idx_name_time_start_time_end does not exist thus not dropping it"'
+	) FROM information_schema.TABLE_CONSTRAINTS
+	WHERE CONSTRAINT_NAME = 'idx_name_time_start_time_end' AND TABLE_NAME = 'events'
+);
+
+PREPARE statement FROM @sql;
+EXECUTE statement;
+DEALLOCATE PREPARE statement;
+`,
+			).Error
 		},
 	}
 }
