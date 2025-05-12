4475f817271cbab321948f9d79134ec8723e258b
iChemy <controlyuu.dev@gmail.com>
2025-05-12 22:07:32 +0900

    :clown_face: use group id for group convert

--- infra/db/converter.go
+++ infra/db/converter.go
@@ -323,6 +323,7 @@ func convEventTodomainEvent(src Event) (dst domain.Event) {
 	dst.Description = src.Description
 	dst.Room = convRoomTodomainRoom(src.Room)
 	dst.Group = convGroupTodomainGroup(src.Group)
+	dst.Group.ID = src.GroupID
 	dst.TimeStart = src.TimeStart
 	dst.TimeEnd = src.TimeEnd
 	dst.CreatedBy = convUserTodomainUser(src.CreatedBy)
