30f4d8c59d32558a47e7ef26f3fc1667f9753906
masky5859 <mathsuky2406@gmail.com>
2026-05-24 17:15:25 +0900

    fix: domainへの変換でprivilegedの値が抜けていた

--- infra/db/converter.go
+++ infra/db/converter.go
@@ -412,6 +412,7 @@ func convTagTodomainTag(src Tag) (dst domain.Tag) {
 }
 func convUserTodomainUser(src User) (dst domain.User) {
 	dst.ID = src.ID
+	dst.Privileged = src.Privilege
 	dst.State = src.State
 	dst.Provider = &domain.Provider{
 		Issuer:  src.Provider.Issuer,
